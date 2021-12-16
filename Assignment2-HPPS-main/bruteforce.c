34b9bd42e07d7786bd1c3f2a97975e6c13a506b0
lizz-ctrl <78913323+lizz-ctrl@users.noreply.github.com>
2021-12-16 21:01:28 +0100

    Add files via upload

---
+++ Assignment2-HPPS-main/bruteforce.c
@@ -0,0 +1,25 @@
+#include "bruteforce.h"
+#include "util.h"
+#include <stdlib.h>
+#include <assert.h>
+#include<stdio.h>
+
+int* knn(int k, int d, int n, const double *points, const double* query) {
+    int *k_array = malloc(k*sizeof(int));
+    for (int i = 0; i < k; i++) {
+        k_array[i] = -1;
+    }
+
+    for (int i = 0; i < n; i++){
+        // printf("%d\n", i);
+        insert_if_closer(k, d, points, k_array, query, i);
+    }
+    // free(k_array);
+    return k_array;
+    free(k_array);
+}
+
+// int* main(const double *points, const double* query) {
+//     int* myk = knn(3, 2, 10, points, query);
+//     return myk
+// }
